07052d798d8bd65ab23c308d5d725f7b9bbc6b55
pedro-mb-rosa <pedro.mb.rosa@ctw.bmwgroup.com>
2023-07-24 23:27:26 +0100

    widgets export file updated

--- lib/utils/widgets/widgets.dart
+++ lib/utils/widgets/widgets.dart
@@ -1,9 +1,10 @@
 export './app_bar.dart';
+export './custom_scroll.dart';
 export './error_widget.dart';
 export './flag_icons.dart';
+export './kyg_button.dart';
+export './kyg_list_tile.dart';
 export './language_picker_widget.dart';
 export './pdf_viewer_page_widget.dart';
-export 'custom_scroll.dart';
-export 'kyg_button.dart';
-export 'timer/timer.dart';
-export 'timer/timer_painter.dart';
+export './timer/timer.dart';
+export './timer/timer_painter.dart';
