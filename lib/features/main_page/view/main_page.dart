a5da45d74b3ccbc96cfede6e47750fcefafa0735
pedro-mb-rosa <pedro.mb.rosa@ctw.bmwgroup.com>
2023-07-24 23:24:23 +0100

    Dummy data leaderboard and changed start button on main page

--- lib/features/main_page/view/main_page.dart
+++ lib/features/main_page/view/main_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:know_your_game/features/main_page/widgets/rule_book_button.dart';
 import 'package:know_your_game/features/questions_page/view/question_page.dart';
+import 'package:know_your_game/utils/dummy/dummy_user.dart';
 import 'package:know_your_game/utils/utils.dart';
 import 'package:know_your_game/utils/widgets/kyg_container.dart';
+import 'package:know_your_game/utils/widgets/kyg_list_tile.dart';
 
 class MainPage extends ConsumerStatefulWidget {
   const MainPage({super.key});
@@ -32,7 +35,22 @@ class _MainPageState extends ConsumerState<MainPage> {
                     RuleBookButton(),
                     Row(
                       children: [
-                        Expanded(child: KygContainer(height: MediaQuery.of(context).size.width * 0.75),),
+                        Expanded(
+                          child: KygContainer(
+                            height: MediaQuery.of(context).size.width * 0.75,
+                            padding: EdgeInsets.symmetric(horizontal: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                KygListTile(user: dummyUser),
+                                KygListTile(user: dummyUser),
+                                KygListTile(user: dummyUser),
+                                KygListTile(user: dummyUser),
+                                KygListTile(user: dummyUser),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                     KygButton(
@@ -40,11 +58,24 @@ class _MainPageState extends ConsumerState<MainPage> {
                         ref.read(firstTimeQuestionScreenProvider.notifier).state = true;
                         context.go(RoutesEndpoints.questions);
                       },
-                      width: MediaQuery.of(context).size.width * 0.25,
-                      child: Text(
-                        "Começar",
-                        style: textTheme.bodyLarge,
-                        textAlign: TextAlign.center,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      color: ColorsUiTheme.mainGreen,
+                      borderColor: Colors.transparent,
+                      boxShadow: [
+                        BoxShadow(
+                          color: ColorsUiTheme.shadowGreen,
+                          offset: const Offset(0.0, 3.0),
+                        )
+                      ],
+                      child: Center(
+                        child: Text(
+                          'Começar',
+                          style: GoogleFonts.nunito(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16.0,
+                          ),
+                        ),
                       ),
                     ),
                   ],
