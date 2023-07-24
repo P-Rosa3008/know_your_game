00b4e8f5959343b729cec18c76a08dddd72aa84c
pedro-mb-rosa <pedro.mb.rosa@ctw.bmwgroup.com>
2023-07-24 23:26:14 +0100

    Dummy user variable created

---
+++ lib/utils/dummy/dummy_user.dart
@@ -0,0 +1,14 @@
+import 'package:know_your_game/utils/user/model/user.dart';
+
+final dummyUser = User(
+  id: 'id',
+  username: 'P-Rosa3008',
+  firstName: 'Pedro',
+  lastName: 'Rosa',
+  email: 'pmbr3008@gmail.com',
+  password: 'randompass',
+  correctAnswers: 100,
+  wrongAnswers: 1,
+  avatarUrl: 'randomUrl',
+  xp: 15,
+);
