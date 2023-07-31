ea99551bfea5e11563d30ac0f79cb73eccb8aebd
P-Rosa3008 <pmbr3008@gmail.com>
2023-07-31 23:07:43 +0100

    Changed to single quote

--- lib/utils/routes/routes_endpoints.dart
+++ lib/utils/routes/routes_endpoints.dart
@@ -1,7 +1,7 @@
 class RoutesEndpoints {
-  static final String main = "/main";
-  static final String profile = "/profile";
-  static final String questionsResults = "/questions_results";
-  static final String questions = "/questions";
-  static final String pdfViewer = "/pdfViewer";
+  static final String main = '/main';
+  static final String profile = '/profile';
+  static final String questionsResults = '/questions_results';
+  static final String questions = '/questions';
+  static final String pdfViewer = '/pdfViewer';
 }
